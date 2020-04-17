ce2ccadd311f393c9d9739367d8482bee3683bf1
Gregory Casanova <gregorynewhouse@hotmail.com>
2020-04-17 12:52:10 -0300

    Updates

---
+++ precache-manifest.96e279ae3fe60a2ebd696fdc9b249ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613858c2369bb0c8661c8fd768363404",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f8c51003513b517b0ceb",
+    "url": "/monsters-rolodex/static/css/main.d7d01195.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c51003513b517b0ceb",
+    "url": "/monsters-rolodex/static/js/main.ab218195.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
